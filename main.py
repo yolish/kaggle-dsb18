534aea0e22317755e1ff05848b91e22b30ed1c95
yolish <yolisha@gmail.com>
2018-02-23 19:57:09 +0000

    added support for seeding, checked sanity basic and augmentation after configuratin changes

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import dsbaugment
 import json
 import sys
 import datetime
+import numpy as np
+import time
 
-
-#TODO gpu, debug e2e, optimizer, training optimization, fix seeds,
+#TODO  gpu once available, full documentation, training optimization
 
 if __name__ == "__main__":
-    # general parameters
     config_filename = sys.argv[1]
     with open(config_filename) as json_config_file:
         config = json.load(json_config_file)
@@ -27,32 +27,38 @@ if __name__ == "__main__":
     sanity_basic = actions_config.get("sanity_basic")
     sanity_augment = actions_config.get("sanity_augment")
     visualize = actions_config.get("visualize")
+    seed = actions_config.get('seed')
 
     train_config = config.get("train")
     test_config = config.get("test")
     misc_config = config.get("misc")
     stage = misc_config.get("stage")
 
-    # set seeds
+    if seed:
+        # seed all random instances
+        np.random.seed(42)
+        torch.manual_seed(42)
+
+
     action = "collecting images details"
-    time = dsbutils.start_action(action)
+    start_time = dsbutils.start_action(action)
     imgs_details = dsbutils.collect_imgs_details(dsb_data_path, stage)
-    dsbutils.complete_action(action, time)
+    dsbutils.complete_action(action, start_time)
     #print(imgs_details.sample(3))
 
     action = "creating train and validation datasets"
     validation_frac = 0.1
-    time = dsbutils.start_action(action)
+    start_time = dsbutils.start_action(action)
     labels_file = os.path.join(dsb_data_path, '{}_train_labels.csv'.format(stage))
     train_dataset = NucleiDataset('train', imgs_df=imgs_details, labels_file=labels_file)
     valid_dataset = train_dataset.split(validation_frac, 'validation')
     print("train size: {}, validation size: {}".format(len(train_dataset),
                                                         len(valid_dataset)))
-    dsbutils.complete_action(action, time)
+    dsbutils.complete_action(action, start_time)
 
     if sanity_basic:
         print("performing a basic sanity check")
-        dsbutils.plot_imgs(train_dataset, 6, (22, 27))
+        dsbutils.plot_imgs(train_dataset, 3, (22, 27))
         n_imgs = 3
         selected_idx = random.sample(range(len(train_dataset)), n_imgs)
         for img_idx in selected_idx:
@@ -69,9 +75,10 @@ if __name__ == "__main__":
     if sanity_augment:
         import matplotlib.pyplot as plt
         print("performing an augmentation sanity check")
-        n_imgs = 1
-        selected_idx = [0]#random.sample(range(len(train_dataset)), n_imgs)
+        n_imgs = 2
+        selected_idx = random.sample(range(len(train_dataset)), n_imgs)
         for img_idx in selected_idx:
+            train_dataset.transform = None
             sample = train_dataset[img_idx]
             img_id = sample.get('id')
             img = sample.get('img')
@@ -79,16 +86,15 @@ if __name__ == "__main__":
             borders = sample.get('borders')
             weight_map = sample.get('weight_map')
             print("image and binary mask before transformation. Image shape: {}, mask shape: {}".format(img.shape,mask.shape))
-            plt.subplot(131)
+            plt.subplot(231)
             plt.imshow(img)
-            plt.subplot(132)
+            plt.subplot(232)
             plt.imshow(mask, cmap='gist_gray')
-            plt.subplot(133)
+            plt.subplot(233)
             plt.imshow(borders)
-            plt.show()
 
-        train_dataset.transform = dsbaugment.tramsformations.get("toy_transform")
-        for img_idx in selected_idx:
+
+            train_dataset.transform = dsbaugment.transformations.get("toy_transform")
             sample = train_dataset[img_idx]
             img_id = sample.get('id')
             img = sample.get('img')
@@ -97,16 +103,16 @@ if __name__ == "__main__":
             weight_map = sample.get('weight_map')
             print("image and binary mask after transformation. Image shape: {}, mask shape: {}".format(img.shape,
                                                                                                         mask.shape))
-            plt.subplot(131)
+            plt.subplot(234)
             plt.imshow(dsbaugment.PIL_torch_to_numpy(img))
-            plt.subplot(132)
+            plt.subplot(235)
             plt.imshow(dsbaugment.PIL_torch_to_numpy(mask), cmap='gist_gray')
-            plt.subplot(133)
+            plt.subplot(236)
             plt.imshow(dsbaugment.PIL_torch_to_numpy(borders), cmap='gist_gray')
             plt.show()
 
 
-    evaluate = True
+    evaluate = False
     test = False
     postprocess = False
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d-%H-%M-%S")
@@ -136,7 +142,7 @@ if __name__ == "__main__":
 
         if model_filename is None:
             action = "training a UNet"
-            time = dsbutils.start_action(action)
+            start_time = dsbutils.start_action(action)
             unet = dsbml.train(train_dataset, transformation, n_epochs, batch_size,
                                lr, weight_decay, momentum, weighted_loss, init_weights, use_gpu)
             dsbutils.complete_action(action, time)
@@ -146,20 +152,23 @@ if __name__ == "__main__":
             time = dsbutils.start_action(action)
             train_dataset = NucleiDataset('train', imgs_df=imgs_details, labels_file=labels_file)
             print("train size: {}".format(len(train_dataset)))
-            dsbutils.complete_action(action, time)
+            dsbutils.complete_action(action, start_time)
 
-            action = "training a UNet on the full train dataset"
-            time = dsbutils.start_action(action)
-            unet = dsbml.train(train_dataset, n_epochs, batch_size, lr, weight_decay, momentum)
-            # save the model
-            model_filename = dsb_output_path + "model_" + timestamp + ".pth"
-            torch.save(unet.state_dict(), model_filename)
-            print("model written to to: {}".format(model_filename))
-            model_metatdate_filename = dsb_output_path + "model_config_" + timestamp + ".txt"
-            with open(model_metatdate_filename, 'w') as f:
-                f.write(json.dumps({"model_config":train_config}))
-            print("model metadat written to to: {}".format(model_metatdate_filename))
-            dsbutils.complete_action(action, time)
+            if train_full:
+                action = "training a UNet on the full train dataset"
+                start_time = dsbutils.start_action(action)
+                unet = dsbml.train(train_dataset, n_epochs, batch_size, lr, weight_decay, momentum)
+                dsbutils.complete_action(action, start_time)
+
+            if save_model:
+                model_filename = dsb_output_path + "model_" + timestamp + ".pth"
+                torch.save(unet.state_dict(), model_filename)
+                print("model written to to: {}".format(model_filename))
+                model_metatdate_filename = dsb_output_path + "model_config_" + timestamp + ".txt"
+                with open(model_metatdate_filename, 'w') as f:
+                    f.write(json.dumps({"model_config":train_config}))
+                print("model metadat written to to: {}".format(model_metatdate_filename))
+                dsbutils.complete_action(action, start_time)
 
 
     if (evaluate or test) and unet is None:
@@ -168,38 +177,40 @@ if __name__ == "__main__":
 
     if evaluate:
         action = "making predictions for the validation set"
-        time = dsbutils.start_action(action)
+        start_time = dsbutils.start_action(action)
         predictions, examples = dsbml.test(unet, valid_dataset, postprocess)
-        dsbutils.complete_action(action, time)
+        dsbutils.complete_action(action, start_time)
 
         action = "evaluating prediction"
+        start_time = dsbutils.start_action(action)
         mean_avg_precision_iou = dsbml.evaluate(predictions, valid_dataset, examples=examples)
         print("IoU for validation dataset: {}".format(mean_avg_precision_iou))
-        dsbutils.complete_action(action, time)
+        dsbutils.complete_action(action, start_time)
         if visualize:
             # visually evaluate a few images by comparing images and masks
             dsbutils.plot_predictions(examples, (7, 12))
 
     if test:
         action = "creating the test set"
-        time = dsbutils.start_action(action)
+        start_time = dsbutils.start_action(action)
         test_dataset = NucleiDataset('test', imgs_df=imgs_details)
         print("test size: {}".format(len(test_dataset)))
-        dsbutils.complete_action(action, time)
+        dsbutils.complete_action(action, start_time)
 
         action = "making predictions for the test set"
-        time = dsbutils.start_action(action)
+        start_time = dsbutils.start_action(action)
         predictions, examples = dsbml.test(unet, test_dataset, postprocess)
-        dsbutils.complete_action(action, time)
+        dsbutils.complete_action(action, start_time)
         # visually evaluate a few images by comparing images and masks
-        dsbutils.plot_predictions(examples, (7, 12))
+        if visualize:
+            dsbutils.plot_predictions(examples, (7, 12))
 
         action = "writing the predictions to submission format"
-        time = dsbutils.start_action(action)
+        start_time = dsbutils.start_action(action)
         submission_df = dsbutils.to_submission_df(predictions)
         submission_filename = dsb_output_path + "model_predictions_postprocess_" +str(postprocess) +"_" + timestamp + ".csv"
         submission_df.to_csv(submission_filename)
         print("predictions on tess set written to: {}".format(submission_filename))
-        dsbutils.complete_action(action, time)
+        dsbutils.complete_action(action, start_time)
 
 
